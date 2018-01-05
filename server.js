4311cf3e8b0475da0d10447a909c6d1d7a037a11
Dan Volosnikov <danvolosnikov@gmail.com>
2018-01-05 14:30:37 -0500

    heroku

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
-// const favicon = require('serve-favicon');
+const favicon = require('serve-favicon');
 const path = require('path');
 const express = require('express');
 const app = express();
 
 app.use('/public', express.static(process.cwd() + '/public'));
-// app.use('/styles', express.static(process.cwd() + '/public/css'));
-// app.use('/images', express.static(process.cwd() + '/public/images'));
-// app.use('/scripts', express.static(process.cwd() + '/public/scripts'));
+app.use('/styles', express.static(process.cwd() + '/public/css'));
+app.use('/images', express.static(process.cwd() + '/public/media'));
+app.use('/scripts', express.static(process.cwd() + '/public/js'));
 // app.use('/fonts', express.static(process.cwd() + '/public/fonts'));
-// app.use(favicon(path.join(__dirname,'public','images','favicon.ico')));
+app.use(favicon(path.join(__dirname,'public','favicon.ico')));
 
 app.get('/', function(req, res){
 			res.sendFile(process.cwd() + '/public/index.html');
